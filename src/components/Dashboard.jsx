11737a3c9986b1305aa87b5f441f0d1ba8b87853
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-12 16:08:07 -0400

    Options component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,12 @@
+import Options from "./Options"
 import styles from "./dashboard.module.css"
 
 export default function Dashboard() {
+  
+
   return (
-    <div className={styles.dashboard}>Dashboard</div>
+    <div className={styles.dashboard}>
+      <Options />
+    </div>
   )
 }
---
+++ src/components/Options.jsx
@@ -0,0 +1,5 @@
+export default function Options() {
+  return (
+    <div>Options</div>
+  )
+}
