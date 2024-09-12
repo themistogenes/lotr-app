d03d8bc5a6e3264b03269abcec9600d4b98470e6
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-12 16:29:16 -0400

    useFetchData hook to fetch data from API

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,16 @@
+import { useState, useEffect } from "react"
+import useFetchData from '../hooks/useFetchData'
 import Options from "./Options"
 import styles from "./dashboard.module.css"
 
 export default function Dashboard() {
-  
+  const { data, loading, error } = useFetchData();
+
+  const questions = [
+    'character',
+    'movie',
+    'book'
+  ]
 
   return (
     <div className={styles.dashboard}>
---
+++ src/hooks/useFetchData.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react"
+
+export default function useFetchData() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const apiUrl = 'https://the-one-api.dev/v2';
+  const APITOKEN = import.meta.env.API_KEY;
+
+  const headers = {
+    'Accept': 'application/json',
+    'Authorization': `Bearer ${APITOKEN}`
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const url = apiUrl + '/' + 'book';
+      try {
+        const res = await fetch(url, {headers: headers});
+        const jsonData = await res.json();
+        console.log('DATA: ', jsonData);
+        setData(data);
+      } catch (err) {
+        setError(err.message);      
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [])
+
+  return {data, error, loading}
+}
